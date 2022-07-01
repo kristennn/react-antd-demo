8936b1294fac8ac271390e54fbab1c682664f233
kristennn <13949836783@163.com>
2022-07-01 18:40:07 +0800

    fix:deal with login page bg css

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -5,7 +5,7 @@ import { Button, Checkbox, Form, Input } from 'antd';
  * @Author: KristenZheng kristen@electracharger.com
  * @Date: 2022-06-29 10:12:54
  * @LastEditors: KristenZheng kristen@electracharger.com
- * @LastEditTime: 2022-07-01 18:34:40
+ * @LastEditTime: 2022-07-01 18:39:44
  * @FilePath: /demo/src/pages/login/index.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -20,7 +20,6 @@ const Login: React.FC = () => {
   const handleLogin = () => {};
   return (
     <div className='lu-login-bg'>
-      <div className='lu-login-cover'></div>
       <img
         className='lu-login-bg-img'
         src={require('../../assets/img/bg.jpg')}
